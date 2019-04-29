467a29c0bb4ad2ab719a46a86bb82bcfd3393ab2
bl00dhound <bloodhound1982@gmail.com>
2019-04-29 12:21:57 +0300

    add authorize

---
+++ middlewares/authorize.js
@@ -0,0 +1,10 @@
+const R = require('ramda');
+const httpErrors = require('http-errors');
+
+const roles = require('../enums/roles');
+
+module.exports = (acceptedRoles = []) => ({ user }, res, next) => {
+	if (R.isEmpty(acceptedRoles) || user.roles === roles.admin) return next();
+	if (R.isEmpty(user) || acceptedRoles.includes(roles.admin)) throw httpErrors.Unauthorized();
+	return next();
+};
