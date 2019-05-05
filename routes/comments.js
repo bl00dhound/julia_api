9b3bcb982e56963c32c84fb329ef61069386922e
bl00dhound <bloodhound1982@gmail.com>
2019-05-05 17:12:33 +0300

    add route for deleting comments

--- routes/comments.js
+++ routes/comments.js
@@ -10,6 +10,12 @@ router.post('/', authorize([roles.user]), (req, res, next) => {
 		.catch(next);
 });
 
+router.delete('/:comment_id', authorize([roles.user]), (req, res, next) => {
+	return service.remove(req.params.comment_id, req.user)
+		.then(comment => res.json(comment))
+		.catch(next);
+});
+
 router.get('/:article_id', (req, res, next) => {
 	return service.list(req.query, req.params.article_id)
 		.then(data => res.json(data))
